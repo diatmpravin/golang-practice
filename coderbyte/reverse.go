06523bf8d076e32149bed131be38dab2e0703efe
Pravin Mishra <pravinmishra88@gmail.com>
2016-06-03 11:39:28 +0530

    reverse strings

--- coderbyte/reverse.go
+++ coderbyte/reverse.go
@@ -1,3 +1,17 @@
 package reverse
 
+import (
+	"strings"
+)
+
+func firstReverse(str string) string {
+	newStr := make([]string, len(str))
+	arry := strings.Split(str, "")
+	
+	for i, c := range arry{
+		newStr[len(arry)-(i+1)] = c
+	}
+
+	return strings.Join(newStr, "")
+}
 
--- coderbyte/reverse_test.go
+++ coderbyte/reverse_test.go
@@ -2,7 +2,20 @@ package reverse
 
 import (
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFirstReverse(t *testing.T){
+	var reverseTest = []struct{
+		in, out string
+	}{
+		{"Hello", "olleH"},
+		{"世界", "界世"},
+		{"", ""},
+	}
+
+	for _, v := range reverseTest {
+		result := firstReverse(v.in)	
+		assert.Equal(t, result, v.out)
+	}	
 }
