841b2cac9d1c9a279e19f4a0c6dc50db2aa6acfa
Pravin Mishra <pravinmishra88@gmail.com>
2016-06-10 08:58:41 +0530

    round time.Now in millisecond

--- mis/time/time.go
+++ mis/time/time.go
@@ -1,27 +1,11 @@
 package main
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	p := fmt.Println
-
 	t := time.Now()
-	t1, _ := time.Parse(
-		time.RFC3339,
-		time.Now())
-	p(t1)
-
-	p(t.Format("2006-01-02T15:04:05.999999-07:00"))
-	//form := "3 04 PM"
-	//t2, e := time.Parse(form, "8 41 PM")
-	//p(t2)
-
-	//fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
-	//	t.Year(), t.Month(), t.Day(),
-	//	t.Hour(), t.Minute(), t.Second())
-
-	//ansic := "Mon Jan _2 15:04:05 2006"
-	//_, e = time.Parse(ansic, "8:41PM")
-	//p(e)
+	fmt.Println(t.Round(time.Millisecond).Format("15:04:05.999999999"))
 }
