fa350c3d2ec6c94ad42e09b7761b459066ce6cf1
Pravin Mishra <pravinmishra88@gmail.com>
2016-06-29 07:03:40 +0530

    completed day-11

--- hackerrank/Golang/tutorial/day-11.go
+++ hackerrank/Golang/tutorial/day-11.go
@@ -1,23 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+var highscore = -1000
+
+func check(m, n int, a [6][6]int) int {
+	var res int = 0
+	var tmp int = n
+	if m < 4 && n < 4 {
+		for x := 0; x < 3; x++ {
+			if x == 1 {
+				res += a[tmp][m+1]
+			} else {
+				res += a[tmp][m] + a[tmp][m+1] + a[tmp][m+2]
+			}
+			tmp += 1
+		}
+		return res
+	}
+	return -100
+}
 
 func main() {
-	A := [6][6]int{}
+	var arr = [6][6]int{}
 
-	for i := 0; i < 6; i++ {
-		for j := 0; j < 6; j++ {
-			var val int
-			fmt.Scan(&val)
-			A[i][j] = val
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr[j]); j++ {
+			fmt.Scan(&arr[i][j])
 		}
 	}
-	fmt.Println(A)
-	fmt.Println()
-
-	for i := 0; i < 4; i++ {
-		for j := 0; j < 4; j++ {
-			
+	var s int
+	for x := 0; x < len(arr); x++ {
+		for y := 0; y < len(arr[x]); y++ {
+			s = check(x, y, arr)
+			if s > highscore {
+				highscore = s
+			}
 		}
 	}
+	fmt.Println(highscore)
 }
