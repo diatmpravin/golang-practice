12c0822a2be7309927fbb5270258fba5056be283
Pravin Mishra <pravinmishra88@gmail.com>
2016-07-12 09:14:44 +0530

    rework on chocolate feast

--- hackerrank/Golang/Algorithms/Implementation/sherlock_and_squares.go
+++ hackerrank/Golang/Algorithms/Implementation/sherlock_and_squares.go
@@ -13,13 +13,26 @@ func main() {
 		var a, b int64
 		fmt.Scan(&a, &b)
 
-		b_sqrt := int64(math.Sqrt(float64(b)))
-		a_sqrt := int64(math.Sqrt(float64(a)))
+		// b_sqrt := int64(math.Sqrt(float64(b)))
+		// a_sqrt := int64(math.Sqrt(float64(a)))
 
-		count := b_sqrt - a_sqrt
-		if math.Sqrt(float64(a))-float64(a_sqrt) == 0 {
-			count++
+		// count := b_sqrt - a_sqrt
+		// if math.Sqrt(float64(a))-float64(a_sqrt) == 0 {
+		// 	count++
+		// }
+		// fmt.Println(count)
+
+		var count, i int64
+		for i = a; i <= b; i++ {
+			if math.Sqrt(float64(i)) - float64(int64(math.Sqrt(float64(i)))) == 0 {
+				count++
+			}
 		}
+
+		// if a == 1 {
+		// 	count++
+		// }
+
 		fmt.Println(count)
 	}
 }
