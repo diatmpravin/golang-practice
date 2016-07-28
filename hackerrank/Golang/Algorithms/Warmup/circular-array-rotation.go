02adac5f4e22e66c77fd94bb1a7c0ab28314374a
Pravin Mishra <pravinmishra88@gmail.com>
2016-07-28 09:00:05 +0530

    circular array rotation

--- hackerrank/Golang/Algorithms/Warmup/circular-array-rotation.go
+++ hackerrank/Golang/Algorithms/Warmup/circular-array-rotation.go
@@ -1,2 +1,21 @@
-package main 
+package main
 
+func findElement(s []int, index int) int {
+	return s[index]
+}
+
+func circularArrayRotation(a []int, k int) []int {
+	for i := 0; i < k; i++ {
+		newSlice := make([]int, len(a))
+		for index, _ := range a {
+			if index == len(a)-1 {
+				newSlice[0] = a[index]
+			} else {
+				newSlice[index+1] = a[index]
+			}
+		}
+		a = newSlice
+	}
+
+	return a
+}
--- hackerrank/Golang/Algorithms/Warmup/circular-array-rotation_test.go
+++ hackerrank/Golang/Algorithms/Warmup/circular-array-rotation_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -8,23 +9,50 @@ import (
 var circularArrayRotationTest = []struct {
 	slice, newSlice []int
 	k               int
+	eq              bool
+	index           int
+	r               int
 }{
-	{[]int{}, []int{}, 5},
-	{nil, []int{}, 5},
+	{[]int{}, []int{}, 5, true, 8, 0},
+	{[]int{0}, []int{}, 5, false, 2, 0},
+	{nil, []int{1}, 5, false, 0, 0},
+	{nil, nil, 5, true, 0, 0},
+	{[]int{1, 2, 3}, []int{2, 3, 1}, 2, true, 1, 3},
+	{[]int{1, 2, 3}, []int{1, 2, 3}, 3, true, 2, 3},
 }
 
 func testEqual(a, b []int) bool {
-	if len(a) == 0 || len(b) == 0 {
+	fmt.Println(a, b)
+	if len(a) == 0 && len(b) == 0 {
 		return true
 	}
 
-	return false 
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i, _ := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
 }
 
 func TestCircularArrayRotation(t *testing.T) {
 	for _, v := range circularArrayRotationTest {
-		// r := circularArrayRotation(v.slice, v.k)
-		flag := testEqual(v.newSlice, v.slice)
-		assert.Equal(t, true, flag)
+		r := circularArrayRotation(v.slice, v.k)
+		flag := testEqual(v.newSlice, r)
+		assert.Equal(t, v.eq, flag)
+	}
+}
+
+func TestFindElement(t *testing.T) {
+	for _, v := range circularArrayRotationTest {
+		if len(v.newSlice) > 0 && v.eq {
+			r := findElement(v.newSlice, v.index)
+			assert.Equal(t, v.r, r)
+		}
 	}
 }
