342e23841cf51f2ec5ad1a54fd2da19ed2b82210
diatmpravin <pravinmishra88@gmail.com>
2016-08-01 10:28:56 +0530

    done kangaroo

--- hackerrank/Golang/Algorithms/Implementation/kangaroo.go
+++ hackerrank/Golang/Algorithms/Implementation/kangaroo.go
@@ -9,15 +9,8 @@ func willTheyMeet(x1, v1, x2, v2 int) string {
 		return "NO"
 	}
 
-	for i, j := x1, x2; ; i, j = i+v1, j+v2 {
-
-		if j > 10001 {
-			break
-		}
-
-		if i == j {
-			return "YES"
-		}
+	if (x1 - x2) % (v2 - v1) == 0  {
+		return "YES"
 	}
 
 	return "NO"
