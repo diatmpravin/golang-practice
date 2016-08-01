024aaa0010f24e7d9fd93177c5f767cb797a5d1b
diatmpravin <pravinmishra88@gmail.com>
2016-08-01 10:12:44 +0530

    working kangaroo

--- hackerrank/Golang/Algorithms/Implementation/kangaroo.go
+++ hackerrank/Golang/Algorithms/Implementation/kangaroo.go
@@ -4,13 +4,12 @@ import (
 	"fmt"
 )
 
-func willTheyMeet(x1, x2, v1, v2 int) string {
-	if x2 > x1 && v2 >= v1 {
+func willTheyMeet(x1, v1, x2, v2 int) string {
+	if x2 > x1 && v2 >= v1  || x1 > x2 && v1 >= v2{
 		return "NO"
 	}
 
-	for i, j := x1, x2; j <= 10000 && i < j; i, j = i+v1, j+v2 {
-		fmt.Println(i, j)
+	for i, j := x1, x2; j <= 10000; i, j = i+v1, j+v2 {
 		if i == j {
 			return "YES"
 		}
@@ -22,5 +21,5 @@ func willTheyMeet(x1, x2, v1, v2 int) string {
 func main() {
 	var x1, v1, x2, v2 int
 	fmt.Scanf("%v %v %v %v", &x1, &v1, &x2, &v2)
-	fmt.Println(willTheyMeet(x1, x2, v1, v2))
+	fmt.Println(willTheyMeet(x1, v1, x2, v2))
 }
