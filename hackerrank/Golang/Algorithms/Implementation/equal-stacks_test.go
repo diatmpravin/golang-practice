48226f1bed071519bdec5e20c165093a38249845
diatmpravin <pravinmishra88@gmail.com>
2016-08-01 12:14:43 +0530

    added few more code

--- hackerrank/Golang/Algorithms/Implementation/equal-stacks_test.go
+++ hackerrank/Golang/Algorithms/Implementation/equal-stacks_test.go
@@ -1,10 +1,20 @@
-package main 
+package main
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestEqualStack(t *testing.T) {
-	
+	var equalStackTest = []struct {
+		slice1, slice2, slice3 []int
+		result                 int
+	}{
+		{[]int{3, 2, 1, 1, 1}, []int{4, 3, 2}, []int{1, 1, 4, 1}, 5},
+	}
+
+	for _, v := range equalStackTest {
+		r := equalStack(v.slice1, v.slice2, v.slice3)
+		assert.Equal(t, v.result, r)
+	}
 }
