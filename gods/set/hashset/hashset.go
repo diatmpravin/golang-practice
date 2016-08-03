5aad7747a35c5ec2f88073013f8504ad23cae649
diatmpravin <pravinmishra88@gmail.com>
2016-08-03 12:39:07 +0530

    added few more test case

--- gods/set/hashset/hashset.go
+++ gods/set/hashset/hashset.go
@@ -7,3 +7,20 @@ type Set struct {
 func NewHashSet() *Set {
 	return &Set{make(map[interface{}]bool)}
 }
+
+func (s *Set) Add(i ...interface{}) {
+	for _, v := range i {
+		_, found := s.set[v]
+		if !found {
+			s.set[v] = true
+		}
+	}
+}
+
+func (s Set) Size() int {
+	return len(s.set)
+}
+
+func (s Set) Empty() bool {
+	return len(s.set) == 0
+}
--- gods/set/hashset/hashset_test.go
+++ gods/set/hashset/hashset_test.go
@@ -8,5 +8,13 @@ import (
 func TestNewSet(t *testing.T) {
 	s := NewHashSet()
 
-	assert.Equal(t, 0, len(s.set), "TestNewSet should be empty")
+	assert.Equal(t, true, s.Empty(), "TestNewSet should be empty")
+}
+
+func TestNewSetWithTwoItem(t *testing.T) {
+	s := NewHashSet()
+
+	s.Add(1)
+	s.Add(2)
+	assert.Equal(t, 2, s.Size(), "TestNewSetWithTwoItem expecting 2 items in set")
 }
