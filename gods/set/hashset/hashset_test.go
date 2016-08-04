c21bd6bfaafec9d879fb9ff8cd2de6f281269864
diatmpravin <pravinmishra88@gmail.com>
2016-08-04 08:36:49 +0530

    Added add testcase

--- gods/set/hashset/hashset_test.go
+++ gods/set/hashset/hashset_test.go
@@ -18,3 +18,18 @@ func TestNewSetWithTwoItem(t *testing.T) {
 	s.Add(2)
 	assert.Equal(t, 2, s.Size(), "TestNewSetWithTwoItem expecting 2 items in set")
 }
+
+func TestNewSetAdd(t *testing.T) {
+	s := NewHashSet()
+	s.Add()
+	s.Add(1)
+	s.Add(2)
+	s.Add(2, 3)
+	s.Add()
+	if actualValue := s.Empty(); actualValue != false {
+		t.Errorf("Got %v expected %v", actualValue, false)
+	}
+	if actualValue := s.Size(); actualValue != 3 {
+		t.Errorf("Got %v expected %v", actualValue, 3)
+	}
+}
